9c9cae9c5d09ae359329cad7709d3c35915614a7
isoumyadas <sammy8das@gmail.com>
2024-07-14 02:17:15 +0530

    Completed with Day1 Challenge

---
+++ Day1/index.js
@@ -0,0 +1,50 @@
+// All About Variables
+
+// Activity 1
+var a = 12;
+console.log(a);
+
+let b = "Soummya";
+console.log(b);
+
+// Activity 2
+const booleanValue = true;
+console.log(booleanValue);
+
+// Activity 3
+
+const stringValue = "Sammy";
+const numberValue = 13;
+const booleanData = false;
+const objectData = {
+  age: 22,
+};
+const arrayValue = ["sammy", 45, true, { city: "boisar" }];
+
+console.log(
+  typeof stringValue,
+  ":::>",
+  typeof numberValue,
+  ":::>",
+  typeof booleanData,
+  ":::>",
+  typeof objectData,
+  ":::>",
+  typeof arrayValue
+);
+
+// Activity 4
+
+let myName = "Soumya ranjan das";
+console.log(myName);
+let changeMyName = "No User";
+myName = changeMyName;
+console.log(changeMyName);
+console.log(myName);
+
+const myyName = "Soumya ranjan das";
+console.log(myName);
+const changeMyyName = "No User";
+myyName = changeMyyName;
+console.log(changeMyyName);
+console.log(myyName);
